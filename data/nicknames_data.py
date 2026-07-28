ce3a013952c6506221986c07a06568b6a907caf2
sotildimashka-prog <sotildimashka@gmail.com>
2026-07-28 11:40:00 +0500

    Update nicknames_data.py

--- data/nicknames_data.py
+++ data/nicknames_data.py
@@ -54,3 +54,46 @@ FEMALE_NICKNAMES = [
     "Chloe",
     "Ariana",
 ]
+
+
+# ---------- 🎮 Foydalanuvchi ismi asosida chiroyli nik generatsiyasi ----------
+
+# {name} o'rniga foydalanuvchi yuborgan ism qo'yiladi. 27 ta shablon - 20+ talabini
+# qamrab oladi.
+NICK_TEMPLATES = [
+    "꧁{name}꧂",
+    "☬{name}☬",
+    "『{name}』",
+    "★彡{name}彡★",
+    "≪{name}≫",
+    "࿐{name}࿐",
+    "☠{name}☠",
+    "亗{name}亗",
+    "๛{name}๛",
+    "❖{name}❖",
+    "⚔{name}⚔",
+    "『ᴳᵒᵈ』{name}",
+    "{name}👑",
+    "ᴹᴿ_{name}",
+    "Mr.{name}",
+    "{name}_YT",
+    "《{name}》",
+    "†{name}†",
+    "☯{name}☯",
+    "‡{name}‡",
+    "『๖ۣۜ{name}』",
+    "ᴸᴼᴿᴰ {name}",
+    "{name}_PRO",
+    "{name}_TM",
+    "『⚡』{name}",
+    "{name}ᶠᶠ",
+    "০{name}০",
+]
+
+
+def generate_custom_nicknames(name: str) -> list:
+    """Foydalanuvchi yuborgan ism asosida 20+ ta chiroyli Free Fire nik yaratadi."""
+    clean_name = name.strip()
+    if not clean_name:
+        return []
+    return [tpl.format(name=clean_name) for tpl in NICK_TEMPLATES]
